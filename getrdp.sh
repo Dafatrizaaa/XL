113c1ec2a020a0b245a2b8bd3488efb7e8813d91
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-18 20:56:39 +0700

    Create getrdp.sh

---
+++ getrdp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#CREATE BY KANGQULL
+#
+echo "Install Windows yang tersedia"
+echo "	1) Windows 2019 (belum tersedia)"
+echo "	2) Windows 2016 (belum tersedia)"
+echo "	3) Windows 2012 (belum tersedia)"
+echo "	4) Windows 10 Pro (tersedia)"
+echo "	5) Windows 11 x Lite Micro"
+
+read -p "Pilih [1]: " GETOS
+
+case "$GETOS" in
+	1|"") GETOS="soon"
+     ;;
+	2) GETOS="soon"
+     ;;
+	3) GETOS="soon"
+     ;;
+	4) GETOS="https://onboardcloud.dl.sourceforge.net/project/vlitee/windows10lite.gz"
+     ;;
+	5) GETOS="soon"
+     ;;
+	*) echo "pilihan salah"; exit;;
+esac
+
+wget --no-check-certificate -O- $GETOS | gunzip | dd of=/dev/vda bs=3M status=progress
+
+echo 'Trimakasih telah menggunakan script by KangQull - Support di https://github.com/KangQull-- Server kamu akan off dalam waktu 15 detik....'
+sleep 15
+poweroff
