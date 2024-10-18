b5c4c89062cc87b80f7108fc1c5fe5bb311e44ee
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-18 23:17:42 +0700

    Update getrdp.sh

--- getrdp.sh
+++ getrdp.sh
@@ -4,6 +4,8 @@
 #
 echo "##################################################"
 echo "#List Pasword Windows>>>>>>>>>>>>>>>>>>>>>>>↓↓   #"
+echo "#-Windows 10&11 login dengan username: Admin↓↓   #"
+echo "#-Windows Server login dengan: Administrator↓↓   #"
 echo "#--------------------------------------     ↓↓   #"
 echo "#|*Windows 2022 : comingsoon          |     ↓↓   #"
 echo "#|*Windows 2019 : comingsoon          |     ↓↓   #"
@@ -16,11 +18,12 @@ echo "#!!WINDOWS CUMA BISA DI VPS UBUNTU DAN DEBIAN!!  #"
 echo "#----------------------------------------------  #"
 echo "#Install Windows yang tersedia dibawah ini:      #"
 echo "##################################################"
-echo "#|1) Windows 2019 (belum tersedia)       |       #"
-echo "#|2) Windows 2016 (belum tersedia)       |       #"
-echo "#|3) Windows 2012 (belum tersedia)       |       #"
-echo "#|4) Windows 10 Pro (tersedia)           |       #"
-echo "#|5) Windows 11 x Lite Micro (tersedia)  |       #"
+echo "#|1) Windows 2022 (belum tersedia)       |       #"
+echo "#|2) Windows 2019 (belum tersedia)       |       #"
+echo "#|3) Windows 2016 (belum tersedia)       |       #"
+echo "#|4) Windows 2012 (belum tersedia)       |       #"
+echo "#|5) Windows 10 Pro (tersedia)           |       #"
+echo "#|6) Windows 11 x Lite Micro (tersedia)  |       #"
 echo "##################################################"
 
 ALAMAT=https://bit.ly
@@ -30,13 +33,15 @@ read -p "Pilih Windows sesuai nomor: " GETOS
 case "$GETOS" in
 	1|"") GETOS="soon"
         ;;
-	2) GETOS="soon"
+	2|"") GETOS="soon"
         ;;
 	3) GETOS="soon"
         ;;
-	4) GETOS="$ALAMAT/3Y9W8zI"
+	4) GETOS="soon"
         ;;
-	5) GETOS="$ALAMAT/4eKLtCs"
+	5) GETOS="$ALAMAT/3Y9W8zI"
+        ;;
+	6) GETOS="$ALAMAT/4eKLtCs"
         ;;
 	*) echo "pilihan salah"; exit;;
 esac
