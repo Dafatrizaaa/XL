271c111d49c0044db313d566f94985b81d7c8686
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-18 22:10:44 +0700

    Update getrdp.sh

--- getrdp.sh
+++ getrdp.sh
@@ -13,7 +13,7 @@ echo "|1) Windows 2019 (belum tersedia)       |"
 echo "|2) Windows 2016 (belum tersedia)       |"
 echo "|3) Windows 2012 (belum tersedia)       |"
 echo "|4) Windows 10 Pro (tersedia)           |"
-echo "|5) Windows 11 x Lite Micro             |"
+echo "|5) Windows 11 x Lite Micro (tersedia)  |"
 echo "#########################################"
 
 read -p "Pilih [1]: " GETOS
@@ -27,7 +27,7 @@ case "$GETOS" in
         ;;
 	4) GETOS="https://onboardcloud.dl.sourceforge.net/project/vlitee/windows10lite.gz"
         ;;
-	5) GETOS="soon"
+	5) GETOS="https://onboardcloud.dl.sourceforge.net/project/vlitee/win11xLiteMicro.gz"
         ;;
 	*) echo "pilihan salah"; exit;;
 esac
