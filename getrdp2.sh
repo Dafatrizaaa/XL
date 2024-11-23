20b918b359d8061be58a71af793f02f158988d1a
KangQull <110822952+KangQull@users.noreply.github.com>
2024-11-23 14:56:59 +0700

    Update getrdp2.sh

--- getrdp2.sh
+++ getrdp2.sh
@@ -141,7 +141,7 @@ fi
 
 echo -e "${RED}Tunggu hingga prosses selesai...${RESET}"
 # Download dan Instal OS dari URL
-wget --no-check-certificate -O- $GETOS | gunzip | dd of=/dev/vda bs=3M status=progress
+wget --no-check-certificate -q -O - $GETOS | gunzip | dd of=/dev/vda bs=3M status=progress
 
 # Mount sistem file Windows
 mount.ntfs-3g /dev/vda2 /mnt
