cd08002a258d79e8f5eb5d5e17a3ed90aea90d04
KangQull <110822952+KangQull@users.noreply.github.com>
2024-11-27 09:22:38 +0700

    Update getrdp2.sh

--- getrdp2.sh
+++ getrdp2.sh
@@ -178,6 +178,7 @@ sc start termservice
 
 cd /d "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup"
 
+
 shutdown /r /f /t 0
 
 del /f /q dpart.bat
@@ -227,6 +228,7 @@ START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
 del /f /q "%SystemDrive%\diskpart.extend"
 
 cd /d "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup"
+
 
 shutdown /r /f /t 0
 
