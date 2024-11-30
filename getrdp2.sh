9e943eb1065bcdbe6f21627308d6e7f770be098a
KangQull <110822952+KangQull@users.noreply.github.com>
2024-11-30 17:34:53 +0700

    Update getrdp2.sh

--- getrdp2.sh
+++ getrdp2.sh
@@ -18,7 +18,7 @@ echo -e "${MAGENTA}2. 🔥  Windows Server 2019      ${CYAN}- Stabilitas dan Kea
 echo -e "${GREEN}3. 🔥  Windows Server 2016      ${CYAN}- Stabilitas dan Performa${RESET}"
 echo -e "${YELLOW}4. 🔥  Windows 10 Pro           ${CYAN}- Windows 10 Spectre Profesional${RESET}"
 echo -e "${RED}5. 🔥  Windows 10 LTSC          ${CYAN}- Untuk Penggunaan Personal${RESET}"
-echo -e "${MAGENTA}6. 🔥  Windows 11 Pro           ${CYAN}- Modern dan Elegan${RESET}"
+echo -e "${MAGENTA}6. 🔥  Windows 11 Pro           ${CYAN}- Windows 11 PRO 23H2${RESET}"
 echo -e "${BLUE}7. 🔥  Windows 11 xLITE         ${CYAN}- Teknologi Terbaru Windows 11 by XLITE${RESET}"
 echo -e "${RED}==========================================${RESET}"
 
@@ -36,7 +36,7 @@ case "$GETOS" in
     3) PASSWORD="comingsoon"; GETOS="soon" ;;
     4) USER="Admin"; IFACE="Ethernet Instance 0 2"; GETOS="$location=WINDOWS10GHOSTSPECTRE$files" ;;
     5) USER="Admin"; IFACE="Ethernet Instance 0 2"; GETOS="$location=NEW10ltsc$files" ;;
-    6) USER="Admin"; IFACE="Ethernet Instance 0 2"; GETOS="NONE" ;;
+    6) USER="Administrator"; IFACE="Ethernet Instance 0 2"; GETOS="http://167.71.221.167/11original.gz" ;;
     7) USER="Admin"; IFACE="Ethernet Instance 0 2"; GETOS="$location=NEW1124H2xLITE$files" ;;
     *) 
         echo "❌ Pilihan tidak valid! Silakan coba lagi."
